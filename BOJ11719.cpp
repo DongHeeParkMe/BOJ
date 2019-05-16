40ce956caa8534adc397ceccf6e69debd5f67235
DongHee Park <DongHeeParkMe@gmail.com>
2019-05-16 19:15:25 +0900

    add BOJ

---
+++ BOJ11719.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+
+
+        while(getline(cin,str)){
+
+            cout<<str<<endl;
+
+        }
+
+
+}
+
